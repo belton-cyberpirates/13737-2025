f69de034960863804e5ab7648b3cc92dbbc31fdd
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2024-11-11 13:13:57 -0600

    add delta time stuff

--- PIDIMUTest.java
+++ PIDIMUTest.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,32 +10,35 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import org.firstinspires.ftc.teamcode.PIDController;
 
-@TeleOp(name="IMU+PID Angle Adjustment Test")
+@TeleOp(name="PIDAngleTest")
 public class PIDIMUTest extends LinearOpMode {
-    // Constants
-    static final double JOYSTICK_ANGLE_MULT = .1;
+	// Constants
+	static final double JOYSTICK_ANGLE_MULT = .1;
 
-    // PID constants
-    double Kp = 0;
-    double Ki = 0;
-    double Kd = 0;
+	// PID constants
+	double Kp = .1;
+	double Ki = 0;
+	double Kd = 0;
 
 	// Drive motors
 	private DcMotorEx BackLeft;
 	private DcMotorEx FrontLeft;
 	private DcMotorEx FrontRight;
 	private DcMotorEx BackRight;
-    
-    // Sensors
+	
+	// Sensors
 	private IMU imu;
 
-    // PID controller
-    PIDController pidController = new PIDController(Kp, Ki, Kd);
+	// PID controller
+	PIDController pidController = new PIDController(Kp, Ki, Kd);
 
-    // Vars
-    double targetAngle = 0;
+	// Vars
+	double targetAngle = 0;
 
 	@Override
 	public void runOpMode() throws InterruptedException {
@@ -43,15 +47,15 @@ public class PIDIMUTest extends LinearOpMode {
 		FrontLeft = hardwareMap.get(DcMotorEx.class, "front_left");
 		FrontRight = hardwareMap.get(DcMotorEx.class, "front_right");
 		BackRight = hardwareMap.get(DcMotorEx.class, "back_right");
-        
-        // Assign sensors
+		
+		// Assign sensors
 		imu = hardwareMap.get(IMU.class, "imu");
 
-        // Init IMU
-        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.mode = BNO055IMU.SensorMode.IMU;
-        parameters.angleUnit = BNO055IMU.AngleUnit.RADIANS;
-        imu.initialize(parameters);
+		// Init IMU
+		// IMU.Parameters parameters = new IMU.Parameters();
+		// parameters.mode = IMU.SensorMode.IMU;
+		// parameters.angleUnit = IMU.AngleUnit.RADIANS;
+		// imu.initialize();
 		
 		// Set zero power behaviours
 		BackLeft.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -60,51 +64,55 @@ public class PIDIMUTest extends LinearOpMode {
 		BackRight.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 		
 		// Set run mode
-		BackLeft.setRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-		FrontLeft.setRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-		FrontRight.setRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODES);
-		BackRight.setRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+		BackLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+		FrontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+		FrontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+		BackRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
 		// Wait for the start button to be pressed
 		waitForStart();
 
+        ElapsedTime timer = new ElapsedTime();
+
 		// Reset robot heading on startup (not initialization)
 		imu.resetYaw();
-
+		
 		while (opModeIsActive()) {
+			
 			// Reset yaw when start button is pressed so that a restart is not needed if the yaw should be reset again.
 			if (gamepad1.start) {
 				imu.resetYaw();
 			}
 
-            double currentAngle = imu.getAngularOrientation().firstAngle;
-            double power = pidController.PIDControlRadians(targetAngle, currentAngle);
+			double currentAngle = imu.getRobotYawPitchRollAngles().getYaw();
+			double power = pidController.PIDControlRadians(targetAngle, currentAngle, timer.seconds());
+            timer.reset();
 
-            BackLeft.setPower(power);
-            FrontLeft.setPower(power);
-            FrontRight.setPower(power);
-            BackRight.setPower(power);
+			BackLeft.setPower(power);
+			FrontLeft.setPower(power);
+			FrontRight.setPower(power);
+			BackRight.setPower(power);
 
-            targetAngle += gamepad1.right_stick_x * JOYSTICK_ANGLE_MULT;
+			targetAngle += gamepad1.right_stick_x * JOYSTICK_ANGLE_MULT;
 
-            if (gamepad1.dpad_up) {
-                targetAngle = 0;
-            }
-            else if (gamepad1.dpad_right) {
-                targetAngle = Math.PI * 0.5;
-            }
-            else if (gamepad1.dpad_down) {
-                targetAngle = Math.PI;
-            }
-            else if (gamepad1.dpad_left) {
-                targetAngle = Math.PI * 1.5;
-            }
+			if (gamepad1.dpad_up) {
+				targetAngle = 0;
+			}
+			else if (gamepad1.dpad_right) {
+				targetAngle = Math.PI * 0.5;
+			}
+			else if (gamepad1.dpad_down) {
+				targetAngle = Math.PI;
+			}
+			else if (gamepad1.dpad_left) {
+				targetAngle = Math.PI * 1.5;
+			}
 
-            telemetry.addData("Current angle", currentAngle);
-            telemetry.addData("Target angle", targetAngle);
+			telemetry.addData("Current angle", currentAngle);
+			telemetry.addData("Target angle", targetAngle);
+			telemetry.addData("Power", power);
 
-            telemetry.update();
-
+			telemetry.update();
         }
-    }
+	}
 }
