2459918af14739c3bc6d7c596e91c93fb23054ca
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2025-02-05 10:16:50 -0600

    indents >:(

--- PIDController.java
+++ PIDController.java
@@ -2,52 +2,52 @@ package org.firstinspires.ftc.teamcode;
 
 
 public class PIDController {
-    double Kp;
-    double Ki;
-    double Kd;
+	double Kp;
+	double Ki;
+	double Kd;
 
-    double integralSum = 0;
-    double lastError = 0;
+	double integralSum = 0;
+	double lastError = 0;
 
-    public PIDController(double Kp, double Ki, double Kd) {
-        setConstants(Kp, Ki, Kd);
-    }
+	public PIDController(double Kp, double Ki, double Kd) {
+		this.Kp = Kp;
+		this.Ki = Ki;
+		this.Kd = Kd;
+	}
 
-    public double PIDControl(double reference, double state, double delta) {
-        double error = reference - state;
-        return PIDControl(error, delta)
-    }
+	public double PIDControl(double reference, double state, double delta) {
+		double error = reference - state;
+		return PIDControl(error, delta);
+	}
 
-    public double PIDControlRadians(double reference, double state, double delta) {
-        double error = angleWrap(reference - state);
-        return PIDControl(error, delta)
-    }
+	public double PIDControlRadians(double reference, double state, double delta) {
+		double error = angleWrap(reference - state);
+		return PIDControl(error, delta);
+	}
 
-    public double PIDControl(double error, double delta) {
-        this.integralSum += error * delta;
-        double derivative = (error - lastError) / delta;
 
-        this.lastError = error;
 
-        double output = (error * this.Kp) + (derivative * this.Kd) + (this.integralSum * this.Ki);
-        return output;
-    }
+	public double PIDControl(double error, double delta) {
+		this.integralSum += error * delta;
+		double derivative = (error - lastError) / delta;
 
-    public double angleWrap(double radians) {
-        while (radians > Math.PI) {
-            radians -= 2 * Math.PI;
-        }
-        
-        while (radians < -Math.PI) {
-            radians += 2 * Math.PI;
-        }
+		this.lastError = error;
 
-        return radians
-    }
+		double output = (error * this.Kp) + (derivative * this.Kd) + (this.integralSum * this.Ki);
+		return output;
+	}
+	
+	
 
-    public void setConstants(double Kp, double Ki, double Kd) {
-        this.Kp = Kp;
-        this.Ki = Ki;
-        this.Kd = Kd;
-    }
+	public double angleWrap(double radians) {
+		while (radians > Math.PI) {
+			radians -= 2 * Math.PI;
+		}
+		
+		while (radians < -Math.PI) {
+			radians += 2 * Math.PI;
+		}
+
+		return radians;
+	}
 }
