8e756ae2041a2ae2b677720717f4da2fe6279a65
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2025-02-07 09:19:55 -0600

    fix not extending action class

--- ActionMove.java
+++ ActionMove.java
@@ -1,26 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
 
-public class ActionMove {
-    Auto auto;
-    double xPos;
-    double yPos;
-    double heading;
+public class ActionMove extends Action {
+	Auto auto;
+	double xPos;
+	double yPos;
+	double heading;
 
-    public ActionMove(Auto auto, double xPos, double yPos, double heading) {
-        this.auto = auto;
-        
-        this.xPos = xPos;
-        this.yPos = yPos;
-        this.heading = heading;
-    }
+	public ActionMove(Auto auto, double xPos, double yPos, double heading) {
+		this.auto = auto;
+		
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.heading = heading;
+	}
 
-    public void onStart() {
-        auto.drivemotors.Move(xPos, yPos, heading);
-    }
-    
-    public boolean isDone() {
-        return auto.driveMotors.isDone();
-    }
+	public void onStart() {
+		auto.driveMotors.Move(xPos, yPos, heading);
+	}
+	
+	public boolean isDone() {
+		return auto.driveMotors.isDone();
+	}
 
 }
