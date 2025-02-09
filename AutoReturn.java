583cdbc6ff0665bb2292441e3608f4c170978ae3
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2025-02-09 03:04:01 -0600

    change AutoOdometry to AutoReturn

--- AutoReturn.java
+++ AutoReturn.java
@@ -7,14 +7,16 @@ import org.firstinspires.ftc.teamcode.BotConfig;
 import org.firstinspires.ftc.teamcode.Auto;
 
 
-@Autonomous(name = "Odometry Test"/*, preselectTeleOp="Field Centric (main)"*/)
-public class AutoOdometry extends Auto {
+@Autonomous(name = "Return", preselectTeleOp="Odometry Test Right")
+public class AutoReturn extends Auto {
 
 	public Action[] getActions() {
 		Action[] actions = {
 			// ======================= AUTO START ======================= //
 
-			new Move(this, 100, 0, 0),
+			new Wait(this, 20000),
+			new Move(this, 0, 0, 0),
+			new Wait(this, 5000),
 			
 			// ======================== AUTO END ======================== //
 		};
