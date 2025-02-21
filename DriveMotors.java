e62705dafd18645545befbe30e0940d8b8965a56
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2025-02-21 12:15:33 -0600

    add power state

--- DriveMotors.java
+++ DriveMotors.java
@@ -26,6 +26,7 @@ public class DriveMotors {
 	enum states {
 		ODOMETRY,
 		DISTANCE,
+		POWER,
 		IDLE
 	}
 
@@ -78,6 +79,48 @@ public class DriveMotors {
 	}
 
 
+	public void InitializeOdometry() {
+		/*
+		Set the odometry pod positions relative to the point that the odometry computer tracks around.
+		The X pod offset refers to how far sideways from the tracking point the
+		X (forward) odometry pod is. Left of the center is a positive number,
+		right of center is a negative number. the Y pod offset refers to how far forwards from
+		the tracking point the Y (strafe) odometry pod is. forward of center is a positive number,
+		backwards is a negative number.
+		 */
+		this.odometry.setOffsets(33, 135); 
+
+		/*
+		Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
+		the goBILDA_SWINGARM_POD, or the goBILDA_4_BAR_POD.
+		If you're using another kind of odometry pod, uncomment setEncoderResolution and input the
+		number of ticks per mm of your odometry pod.
+		 */
+		this.odometry.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+		//odo.setEncoderResolution(13.26291192);
+
+
+		/*
+		Set the direction that each of the two odometry pods count. The X (forward) pod should
+		increase when you move the robot forward. And the Y (strafe) pod should increase when
+		you move the robot to the left.
+		 */
+		this.odometry.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+
+
+		/*
+		Before running the robot, recalibrate the IMU. This needs to happen when the robot is stationary
+		The IMU will automatically calibrate when first powered on, but recalibrating before running
+		the robot is a good idea to ensure that the calibration is "good".
+		resetPosAndIMU will reset the position to 0,0,0 and also recalibrate the IMU.
+		This is recommended before you run your autonomous, as a bad initial calibration can cause
+		an incorrect starting value for x, y, and heading.
+		 */
+		//odo.recalibrateIMU();
+		this.odometry.resetPosAndIMU();
+	}
+
+
 	public void process() {
 		double deltaTime = deltaTimer.seconds();
 
@@ -92,6 +135,17 @@ public class DriveMotors {
 		}
 		
 		odo.update();
+		deltaTimer.reset();
+	}
+
+
+	public void DriveWithPower(double backLeftPower, double frontLeftPower, double frontRightPower, double backRightPower) {
+		this.state = states.POWER;
+
+		backLeft.setPower(backLeftPower);
+		frontLeft.setPower(frontLeftPower);
+		frontRight.setPower(frontRightPower);
+		backRight.setPower(backRightPower);
 	}
 
 
