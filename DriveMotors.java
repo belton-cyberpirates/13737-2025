aa34c0f95b2dcef74685ecd530f6533de5e944b5
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2025-02-25 09:18:54 -0600

    further tune PID loops

--- DriveMotors.java
+++ DriveMotors.java
@@ -30,10 +30,10 @@ public class DriveMotors {
 		IDLE
 	}
 
-	static PIDController distanceSensorPidController = new PIDController(0.007, 0.0005, 0.00018);
-	static PIDController xPosPidController = new PIDController(0.015, 0.00002, 0.0005);
-	static PIDController yPosPidController = new PIDController(0.015, 0.00002, 0.0005);
-	static PIDController imuPidController = new PIDController(.8, 0, 0.005);
+	static PIDController distanceSensorPidController = new PIDController(0, 0, 0);
+	static PIDController xPosPidController = new PIDController(0.0025, 0.000001, 0.000004);
+	static PIDController yPosPidController = new PIDController(0., 0., 0.);
+	static PIDController imuPidController = new PIDController(1, 0, 0.001);
 
 	static Orientation angles;
 
@@ -74,7 +74,7 @@ public class DriveMotors {
 		this.frontRight.setTargetPosition(0);
 		this.backRight.setTargetPosition(0);
 	
-		this.distSensor = auto.hardwareMap.get(DistanceSensor.class, BotConfig.DISTANCE_SENSOR_NAME);
+		//this.distSensor = auto.hardwareMap.get(DistanceSensor.class, BotConfig.DISTANCE_SENSOR_NAME);
 
 		this.odometry = auto.hardwareMap.get(GoBildaPinpointDriver.class, "odo");
 
@@ -174,10 +174,10 @@ public class DriveMotors {
 		rotatedX *= BotConfig.STRAFE_MULT;
 
 		// Set the power of the wheels based off the new movement vector
-		double backLeftPower   = (/* rotatedY - rotatedX +*/ anglePower);
-		double frontLeftPower  = (/* rotatedY + rotatedX +*/ anglePower);
-		double frontRightPower = (/*-rotatedY + rotatedX +*/ anglePower);
-		double backRightPower  = (/*-rotatedY - rotatedX +*/ anglePower);
+		double backLeftPower   = (-rotatedX - rotatedY + anglePower);
+		double frontLeftPower  = (-rotatedX + rotatedY + anglePower);
+		double frontRightPower = ( rotatedX + rotatedY + anglePower);
+		double backRightPower  = ( rotatedX - rotatedY + anglePower);
 
 		// Find highest motor power value
 		double highestPower = Collections.max(Arrays.asList( Math.abs(backLeftPower), Math.abs(frontLeftPower), Math.abs(frontRightPower), Math.abs(backRightPower) ));
@@ -232,7 +232,7 @@ public class DriveMotors {
 	public void Move(double xPos, double yPos, double heading) {
 		this.targetX = xPos;
 		this.targetY = yPos;
-		this.targetHeading = ( heading * ( Math.PI / 180 ) );
+		this.targetHeading = -( heading * ( Math.PI / 180 ) );
 
 		this.odometryTimer.reset();
 
@@ -255,10 +255,10 @@ public class DriveMotors {
 	public boolean isDone() {
 		switch (this.state) {
 			case ODOMETRY:
-				return odometryTimer.milliseconds() > 1000 &&
-					(Math.abs(xPosPidController.lastOutput) < .1) && 
-					(Math.abs(yPosPidController.lastOutput) < .05) && 
-					(Math.abs(imuPidController.lastOutput) < .05);
+				return odometryTimer.milliseconds() > 750 &&
+					(Math.abs(xPosPidController.lastOutput) < 1) && 
+					//(Math.abs(yPosPidController.lastError) < 5) && 
+					(Math.abs(imuPidController.lastError) < .03);
 			
 			case DISTANCE:
 				return (Math.abs(distanceSensorPidController.lastError) < 5);
