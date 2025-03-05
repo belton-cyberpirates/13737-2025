5823ea1c17600248a229d18c7064ac37b58ec28e
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2025-03-04 19:01:32 -0600

    adjust offset values

--- ResetOdometry.java
+++ ResetOdometry.java
@@ -10,7 +10,7 @@ public class ResetOdometry extends LinearOpMode {
 	public void runOpMode() {
 		GoBildaPinpointDriver odometry = hardwareMap.get(GoBildaPinpointDriver.class, "odo");
 		
-		odometry.setOffsets(33, 135);
+		odometry.setOffsets(145, -70); 
 		odometry.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
 		odometry.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.FORWARD, GoBildaPinpointDriver.EncoderDirection.FORWARD);
 		odometry.resetPosAndIMU();
