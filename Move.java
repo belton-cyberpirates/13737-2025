f6e3697c25cb5b2726cf64fd08d99340acc62e9c
Wyatt <150945595+qwertyuiopwm@users.noreply.github.com>
2025-03-05 08:32:56 -0600

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,12 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 
 public class Move extends Action {
 	Auto auto;
 	double xPos;
 	double yPos;
 	double heading;
-
+	double expectedTime = 0;
+	ElapsedTime startTime;
+	
 	public Move(Auto auto, double xPos, double yPos, double heading) {
 		this.auto = auto;
 		
@@ -15,12 +19,22 @@ public class Move extends Action {
 		this.heading = heading;
 	}
 
+	public Move(Auto auto, double xPos, double yPos, double heading, double expectedTime) {
+		this.auto = auto;
+		
+		this.xPos = xPos;
+		this.yPos = yPos;
+		this.heading = heading;
+		this.expectedTime = expectedTime;
+	}
+
 	public void onStart() {
+		startTime = new ElapsedTime();
 		auto.driveMotors.Move(xPos, yPos, heading);
 	}
 	
 	public boolean isDone() {
-		return auto.driveMotors.isDone();
+		return auto.driveMotors.isDone() || (expectedTime != 0 && startTime.time() >= expectedTime);
 	}
 
 }
