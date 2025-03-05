56a31fad26d5fc2a4a6ff4b252520575d84969e7
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2025-03-04 18:59:08 -0600

    adjust auto values

--- AutoLeft.java
+++ AutoLeft.java
@@ -16,18 +16,18 @@ public class AutoLeft extends Auto {
 			// ======================= AUTO START ======================= //
 			
 			// Score starting sample
-			new Move(this, 85, 830, -115),
-			new MoveArm(this, BotConfig.BASKET_HEIGHT, true),
+			new Move(this, 85, 830, -115, 2),
+			new MoveArm(this, BotConfig.BASKET_HEIGHT, true, 0.25),
 			new MoveWrist(this, BotConfig.WRIST_DUNK_HEIGHT, true),
 			new OpenClawWide(this),
 			new Wait(this, 250),
 			
 			// Reset arm and wrist
 			new MoveWrist(this, BotConfig.WRIST_PASSIVE, false),
-			new MoveArm(this, 0, true),
+			new MoveArmWithVelocity(this, 0, true, BotConfig.ARM_VELOCITY/4),
 			
 			// Grab 2nd sample
-			new Move(this, BotConfig.BLOCK_GRAB_X, -BotConfig.FIRST_BLOCK_Y, 0),
+			new Move(this, BotConfig.BLOCK_GRAB_X, -BotConfig.FIRST_BLOCK_Y, 0, 2),
 			new MoveWrist(this, BotConfig.WRIST_SAMPLE_HEIGHT, true),
 			new Wait(this, 250),
 			new CloseClaw(this),
@@ -35,18 +35,18 @@ public class AutoLeft extends Auto {
 			new MoveWrist(this, 0, false),
 			
 			// Score 2nd sample
-			new Move(this, BotConfig.BASKET_X, BotConfig.BASKET_Y, -135),
-			new MoveArm(this, BotConfig.BASKET_HEIGHT, true),
+			new Move(this, BotConfig.BASKET_X, BotConfig.BASKET_Y, -135, 2),
+			new MoveArm(this, BotConfig.BASKET_HEIGHT, true, 0.25),
 			new MoveWrist(this, BotConfig.WRIST_DUNK_HEIGHT, true),
 			new OpenClawWide(this),
 			new Wait(this, 250),
 			
 			// Reset arm and wrist
 			new MoveWrist(this, BotConfig.WRIST_PASSIVE, false),
-			new MoveArm(this, 0, true),
+			new MoveArmWithVelocity(this, 0, true, BotConfig.ARM_VELOCITY/4),
 			
 			// Grab 3rd sample
-			new Move(this, BotConfig.BLOCK_GRAB_X, -BotConfig.SECOND_BLOCK_Y, 0),
+			new Move(this, BotConfig.BLOCK_GRAB_X, -BotConfig.SECOND_BLOCK_Y, 0, 2),
 			new MoveWrist(this, BotConfig.WRIST_SAMPLE_HEIGHT, true),
 			new Wait(this, 250),
 			new CloseClaw(this),
@@ -55,32 +55,41 @@ public class AutoLeft extends Auto {
 			new Wait(this, 500),
 			
 			// Score 3rd sample 
-			new Move(this, BotConfig.BASKET_X, BotConfig.BASKET_Y, -135),
-			new MoveArm(this, BotConfig.BASKET_HEIGHT, true),
+			new Move(this, BotConfig.BASKET_X, BotConfig.BASKET_Y, -135, 2),
+			new MoveArm(this, BotConfig.BASKET_HEIGHT, true, 0.25),
 			new MoveWrist(this, BotConfig.WRIST_DUNK_HEIGHT, true),
 			new OpenClawWide(this),
 			new Wait(this, 250),
 			
 			// Reset arm and wrist
 			new MoveWrist(this, BotConfig.WRIST_PASSIVE, false),
-			new MoveArm(this, 0, true),
+			new MoveArmWithVelocity(this, 0, true, BotConfig.ARM_VELOCITY/4),
 			
 			// Grab 4th sample
-			new Move(this, BotConfig.THIRD_BLOCK_X, -BotConfig.THIRD_BLOCK_Y - 100, -90),
+			new Move(this, BotConfig.THIRD_BLOCK_X, -BotConfig.THIRD_BLOCK_Y - 100, -90, 4),
 			new MoveWrist(this, BotConfig.WRIST_SIDE_SAMPLE_HEIGHT, true),
-			new Move(this, BotConfig.THIRD_BLOCK_X, -BotConfig.THIRD_BLOCK_Y, -90),
+			new Move(this, BotConfig.THIRD_BLOCK_X, -BotConfig.THIRD_BLOCK_Y, -90, 2),
 			new Wait(this, 250),
 			new CloseClaw(this),
 			new Wait(this, 250),
+			new Move(this, BotConfig.THIRD_BLOCK_X, -BotConfig.THIRD_BLOCK_Y - 150, -90, 1),
 			new MoveWrist(this, 0, false),
 			new Wait(this, 500),
 			
 			// Score 4th sample 
-			new Move(this, BotConfig.BASKET_X, BotConfig.BASKET_Y, -135),
-			new MoveArm(this, BotConfig.BASKET_HEIGHT, true),
+			new Move(this, BotConfig.BASKET_X, BotConfig.BASKET_Y, -135, 2),
+			new MoveArm(this, BotConfig.BASKET_HEIGHT, true, 0.25),
 			new MoveWrist(this, BotConfig.WRIST_DUNK_HEIGHT, true),
 			new OpenClawWide(this),
 			new Wait(this, 250),
+			
+			// Reset arm and wrist
+			new MoveWrist(this, BotConfig.WRIST_PASSIVE, true),
+			new MoveArmWithVelocity(this, 0, true, BotConfig.ARM_VELOCITY/4),
+			
+			// Preform a level 1 ascend
+			new MoveWrist(this, 800, false),
+			new Move(this, 1650, 580, 90)
 			
 			
 			// ======================== AUTO END ======================== //
